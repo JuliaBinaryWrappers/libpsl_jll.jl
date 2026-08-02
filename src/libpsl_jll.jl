944f3781ad01b5de8663f007e21ed341b37f8f95
jlbuild <juliabuildbot@gmail.com>
2026-08-02 13:28:54 +0000

    libpsl_jll build 0.23.1+0

--- src/libpsl_jll.jl
+++ src/libpsl_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libpsl")
-JLLWrappers.@generate_main_file("libpsl", UUID("c4fadf96-db99-5883-8c55-41cf0165eeb0"))
+JLLWrappers.@generate_main_file("libpsl", Base.UUID("c4fadf96-db99-5883-8c55-41cf0165eeb0"))
 end  # module libpsl_jll
